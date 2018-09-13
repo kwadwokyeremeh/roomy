971a24e027b26600ef169403ccb7b90c967aa69e
Kwadwo Kyeremeh Tuffour <kyerematics@gmail.com>
2018-09-13 16:01:16 +0000

    Fixed typo

--- app/Notifications/HostellerResetPasswordNotification.php
+++ app/Notifications/HostellerResetPasswordNotification.php
@@ -43,7 +43,7 @@ class HostellerResetPasswordNotification extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('You are receiving this email because we recieved a password reset request from your account')
+                    ->line('You are receiving this email because we received a password reset request from your account')
                     ->action('Reset Password', route('hosteller.password.reset',$this->token))
                     ->line('If you did not request a password reset, no further action is required');
     }
